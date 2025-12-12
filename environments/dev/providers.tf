d9e4ab3a9147d0f9ebb407b4d02230fa1f9b815b
Steven Fisher <steven.fisher@techconsulting.tech>
2025-12-12 12:31:28 -0500

    changed tf backend sa name

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "steven-tfstate-rg"
-    storage_account_name = "steven-tfstateaccount"
+    storage_account_name = "steventfstateaccount"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"   # dev folder
   }
