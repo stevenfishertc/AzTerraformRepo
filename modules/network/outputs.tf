f47f9a9eebe4d11841497980d43b3e2b17012eb6
Steven Fisher <steven.fisher@techconsulting.tech>
2025-12-14 16:17:32 -0500

    uncommented network output vars

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,7 +1,7 @@
-# output "vnet_id" {
-#   value = azurerm_virtual_network.vnet.id
-# }
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
 
-# output "nic_id" {
-#   value = azurerm_network_interface.vm_nic.id
-# }
+output "nic_id" {
+  value = azurerm_network_interface.vm_nic.id
+}
